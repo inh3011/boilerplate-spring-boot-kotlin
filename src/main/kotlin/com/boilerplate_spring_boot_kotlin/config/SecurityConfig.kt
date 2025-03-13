ddcc041f42ed903a31e4489499744cdfe466b918
inh3011 <inh3011@gmail.com>
2025-03-13 23:49:30 +0900

    feat: create security config

---
+++ src/main/kotlin/com/boilerplate_spring_boot_kotlin/config/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package com.boilerplate_spring_boot_kotlin.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth -> auth.anyRequest().permitAll() }
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
--- src/main/kotlin/com/boilerplate_spring_boot_kotlin/entity/User.kt
+++ src/main/kotlin/com/boilerplate_spring_boot_kotlin/entity/User.kt
@@ -13,7 +13,6 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
-
 @Entity
 @Table(name = "Users")
 @EntityListeners(AuditingEntityListener::class)
