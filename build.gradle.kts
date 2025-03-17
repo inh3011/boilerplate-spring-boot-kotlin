8a5453dee450673f2fd99ce2f34da880f2c5fcb8
inh3011 <inh3011@gmail.com>
2025-03-17 23:36:22 +0900

    feat: add modules jjwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("io.jsonwebtoken:jjwt:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     runtimeOnly("org.postgresql:postgresql")
 
